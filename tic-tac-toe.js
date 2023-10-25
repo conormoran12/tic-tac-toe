149494cc7265c20199cddc1c59f8f859bd4a2f1b
Conor Moran <{ID}+{username}@users.noreply.github.com>
2023-10-26 00:26:19 +0100

    Good start on project

---
+++ tic-tac-toe.js
@@ -0,0 +1,30 @@
+(function gameBoard() {
+    this.boxes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.currentPlayer = player1;
+    this.setCurrentPlayer = () => {
+        this.currentPlayer = 
+    }
+}());
+
+function displayController() {
+    this.setMarker = (element) => {
+        
+    }
+
+    return { setMarker };
+}
+
+function player(name, char) {
+    this.name = name;
+    this.char = char;
+    return {name, char};
+}
+
+const player1 = player("Player1", "X");
+const player2 = player("Player2", "O");
+
+document.querySelectorAll(".field").forEach(element => {
+    element.addEventListener("mouseup", (event => {
+        element.innerHTML = "X";
+    }))
+});
